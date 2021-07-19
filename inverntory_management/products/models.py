1de695a00f252f55491459d3613c875d3c562b86
AhtashamUlHasaan <ahtashamulhasaan@A006-00586.local>
2021-07-19 11:35:14 +0500

    Product Crud with Live Search Completed

--- inverntory_management/products/models.py
+++ inverntory_management/products/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=500, blank=True)
     company_name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
--- inverntory_management/products/views.py
+++ inverntory_management/products/views.py
@@ -1,13 +1,16 @@
 
+from .forms.product_form import ProductForm
 from django.http.response import JsonResponse
 from products.models import Product
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.template.loader import render_to_string
 
 from products.models import Product
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template.loader import render_to_string
+
 
 
 def index(request):
@@ -38,3 +41,60 @@ def product_search(request):
     data_dict = {'html_from_view': data}
     return JsonResponse(data=data_dict, safe=False)
 
+def detail(request, prod_id):
+    try:
+        product = Product.objects.get(pk=prod_id)
+    except Product.DoesNotExist:
+        return render(request, 'products/error.html', {'error': {'error_code':404, 'error_message':'Product not found '}})
+
+    return render(request, 'products/details.html', {'prod': product})
+
+def product_search(request):
+    url_param = request.GET.get('q')
+    if url_param:
+        products = Product.objects.filter(name__icontains=url_param)
+    else:
+        products = Product.objects.all()
+    
+    data = render_to_string(
+        template_name = 'products/products_partial.html',
+        context = {'products':products}
+    )
+    data_dict = {'html_from_view': data}
+    return JsonResponse(data=data_dict, safe=False)
+
+def update_product(request):
+    # id = request.post.get('id')
+    # prod = Product.objects.get(pk=id)
+    # prod.name = request.post.get('name')
+    # prod.company_name = request.post.get('company_name')
+    # prod.price = request.post.get('price')
+    # prod.description = request.post.get('description')
+    # # save modified product
+    # prod.save()
+
+    if request.method == 'POST':
+        id = request.GET.get('q')
+        model = Product.objects.get(pk=id)
+        form = ProductForm(request.POST, instance=model)
+        # check whether it's valid:
+        if form.is_valid():
+            form.save()
+            return redirect('/products/')
+    else:
+        id = request.GET.get('q')
+        prod = Product.objects.get(pk=id)
+        form = ProductForm(instance=prod)
+        return render(request, 'products/edit.html', {'form': form, 'ctx':str(id)})
+
+def add_product(request):
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = ProductForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            form.save()
+            return redirect('/products/')
+    else:
+        form = ProductForm()
+        return render(request, 'products/edit.html', {'form': form})
