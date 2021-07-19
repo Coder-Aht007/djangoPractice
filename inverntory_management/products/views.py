348e51bac02b361eb8e98fff7a6bf074a282c68a
Mohammad Ahtasham <mohammad.ahtasham@arbisoft.com>
2021-07-19 17:01:09 +0500

    Error template added

--- inverntory_management/products/views.py
+++ inverntory_management/products/views.py
@@ -2,16 +2,17 @@ from products.models import Product
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 
 def index(request):
     products = Product.objects.order_by('-created_at')
-    context = {"products":products}
+    context = {"products": products}
     return render(request, 'products/index.html', context)
+
 
 def detail(request, prod_id):
     try:
         product = Product.objects.get(pk=prod_id)
     except Product.DoesNotExist:
-        raise Http404("Product does not exist")
+        return render(request, 'products/error.html', {'error': {'error_code':404, 'error_message':'Product not found '}})
+
     return render(request, 'products/details.html', {'prod': product})
