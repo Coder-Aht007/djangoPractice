5a2c1945729f3b2b45b19cd6f35d051932560a3d
AhtashamUlHasaan <ahtashamulhasaan@A006-00586.local>
2021-07-19 15:30:33 +0500

    Product Crud Completed

--- inverntory_management/products/models.py
+++ inverntory_management/products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500, blank=True)
+    company_name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    created_by = models.ForeignKey(
+        User, related_name='products', on_delete=models.CASCADE, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.name
--- inverntory_management/products/views.py
+++ inverntory_management/products/views.py
@@ -1,8 +1,17 @@
+from products.models import Product
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the products index.")
+    products = Product.objects.order_by('-created_at')
+    context = {"products":products}
+    return render(request, 'products/index.html', context)
 
+def detail(request, prod_id):
+    try:
+        product = Product.objects.get(pk=prod_id)
+    except Product.DoesNotExist:
+        raise Http404("Product does not exist")
+    return render(request, 'products/details.html', {'prod': product})
