6c08cb4fb56bcc128fce22eafee2afca0ca63c0f
Coder-Aht007 <60315450+Coder-Aht007@users.noreply.github.com>
2021-07-23 15:32:11 +0500

    Live search (#3)
    * products_app_initialized
    
    * Product Crud Completed
    
    * Error template added
    
    * Live search added
    
    * delay added to ajax call for Server Efficiency
    
    * gitIgnore added
    
    Co-authored-by: AhtashamUlHasaan <ahtashamulhasaan@A006-00586.local>
    Co-authored-by: Mohammad Ahtasham <mohammad.ahtasham@arbisoft.com>

--- inverntory_management/products/models.py
+++ inverntory_management/products/models.py
@@ -7,8 +7,6 @@ class Product(models.Model):
     description = models.CharField(max_length=500, blank=True)
     company_name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    created_by = models.ForeignKey(
-        User, related_name='products', on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
--- inverntory_management/products/urls.py
+++ inverntory_management/products/urls.py
@@ -7,4 +7,5 @@ app_name = "products"
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:prod_id>/', views.detail, name='detail'),
+    path('search/', views.product_search, name="search")
 ]
