8c33b5e2d71f920ede4da23466de486bb631a011
Coder-Aht007 <60315450+Coder-Aht007@users.noreply.github.com>
2021-07-27 15:44:14 +0500

    Routes secured, Authentications Added,Custom User Model Added (#5)
    Co-authored-by: AhtashamUlHasaan <ahtashamulhasaan@A006-00586.local>

--- inverntory_management/inverntory_management/urls.py
+++ inverntory_management/inverntory_management/urls.py
@@ -20,4 +20,5 @@ from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
