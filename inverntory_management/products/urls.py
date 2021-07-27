0eb5d8b3a09730f6a10c9fa1af23dad55322f4ed
Coder-Aht007 <60315450+Coder-Aht007@users.noreply.github.com>
2021-07-27 15:23:40 +0500

    Product Crud with Live Search Completed (#4)
    Co-authored-by: AhtashamUlHasaan <ahtashamulhasaan@A006-00586.local>

--- inverntory_management/products/urls.py
+++ inverntory_management/products/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path
-
 from . import views
+from django.urls import path
 
 app_name = "products"
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:prod_id>/', views.detail, name='detail'),
-    path('search/', views.product_search, name="search")
+    path('search/', views.product_search, name="search"),
+    path('update/',views.update_product, name='update'),
+    path('delete',views.delete_product, name='delete'),
+    path('add/', views.add_product, name='add')
 ]
